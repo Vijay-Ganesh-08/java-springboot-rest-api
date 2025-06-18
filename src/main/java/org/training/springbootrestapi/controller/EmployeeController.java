7705b82d2edb2542939526858600c2c67027acd0
Vijay Ganesh <vijay.ganesh0811@gmail.com>
2025-06-18 12:44:47 +0530

    Using Consumes and Produces

--- src/main/java/org/training/springbootrestapi/controller/EmployeeController.java
+++ src/main/java/org/training/springbootrestapi/controller/EmployeeController.java
@@ -1,8 +1,6 @@
 package org.training.springbootrestapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.*;
 import org.training.springbootrestapi.model.Employee;
@@ -17,27 +15,25 @@ public class EmployeeController {
     EmployeeRepo employeeRepo;
 
     @GetMapping(path="getEmployees",produces = {"application/json","application/xml"})
-    @ResponseBody
     public List<Employee> getAliens() {
         List<Employee> list =  employeeRepo.findAll();
         return list;
     }
 
-    @GetMapping("getEmployee")
+    @GetMapping(path ="getEmployee", produces = {"application/json","application/xml"})
     public Employee getEmployees(@RequestParam int eid) {
         Employee employee = employeeRepo.findById(eid).orElse(new Employee(0,"",""));
         return employee;
     }
 
-    @GetMapping("getEmployee/{eid}")
+    @GetMapping(path="getEmployee/{eid}", produces = {"application/json","application/xml"})
     public Employee getEmployee(@PathVariable("eid") int eid) {
         return employeeRepo.findById(eid).orElse(new Employee(0,"",""));
     }
 
-    @PostMapping("addEmployee")
-    public Employee addEmployee(Employee employee) {
+    @PostMapping(path = "addEmployee", consumes = {"application/json","application/xml"})
+    public Employee addEmployee(@RequestBody Employee employee) {
         return employeeRepo.save(employee);
     }
 
-//    @PutMapping
 }
